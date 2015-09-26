0cdd0826d2e7ed3e70c19e57c066942da7ade00e
jjcarlton <josh.carlton8@gmail.com>
2015-09-25 19:09:44 -0700

    Completed seg7 and started work on datapath

--- Roulette/digit7seg.vhd
+++ Roulette/digit7seg.vhd
@@ -25,7 +25,31 @@ END;
 
 ARCHITECTURE behavioral OF digit7seg IS
 BEGIN
+  
+  process(all)
+    begin
+    case digit is
+      when "0000" => seg7 <= "1000000";
+      when "0001" => seg7 <= "1111001";
+      when "0010" => seg7 <= "0100100";
+      when "0011" => seg7 <= "0110000";
+      when "0100" => seg7 <= "0011001";
+      when "0101" => seg7 <= "0010010";
+      when "0110" => seg7 <= "0000010";
+      when "0111" => seg7 <= "1111000";
+      when "1000" => seg7 <= "0000000";
+      when "1001" => seg7 <= "0010000";
+      when "1010" => seg7 <= "0001000";
+      when "1011" => seg7 <= "0000011";
+      when "1100" => seg7 <= "1000110";
+      when "1101" => seg7 <= "0100001";
+      when "1110" => seg7 <= "0000110";
+      when "1111" => seg7 <= "0001110";
+      when others => seg7 <= "1111111";
+  end case;
+      
+  end process;
+  
 
--- Your code goes here
 
 END;
--- Roulette/roulette.vhd
+++ Roulette/roulette.vhd
@@ -35,5 +35,98 @@ END roulette;
 
 
 ARCHITECTURE structural OF roulette IS
- --- Your code goes here
+  
+  component digit7seg
+    port (digit : in unsigned(3 downto 0); seg7 : out std_logic_vector(6 downto 0) );
+  end component;
+  
+  component win
+	 port (spin_result_latched : in unsigned(5 downto 0);  -- result of the spin (the winning number)
+             bet1_value : in unsigned(5 downto 0); -- value for bet 1
+             bet2_colour : in std_logic;  -- colour for bet 2 0 is black 1 is red
+             bet3_dozen : in unsigned(1 downto 0);  -- dozen for bet 3
+             bet1_wins : out std_logic;  -- whether bet 1 is a winner
+             bet2_wins : out std_logic;  -- whether bet 2 is a winner
+             bet3_wins : out std_logic); -- whether bet 3 is a winner
+  end component;
+  
+  component spinwheel
+	 port(
+		fast_clock : IN  STD_LOGIC;  -- This will be a 27 Mhz Clock
+		resetb : IN  STD_LOGIC;      -- asynchronous reset
+		spin_result  : OUT UNSIGNED(5 downto 0));  -- current value of the wheel
+  end component;
+  
+  
+  
+  signal digit : unsigned(3 downto 0);
+  signal balance : unsigned(11 downto 0);
+  
+  signal bet1_value : unsigned(5 downto 0);
+  signal bet2_colour : std_logic;
+  signal bet3_dozen : unsigned(1 downto 0);
+  signal bet1_wins : std_logic;
+  signal bet2_wins : std_logic;
+  signal bet3_wins : std_logic;
+  signal spin_result : unsigned(5 downto 0);
+  signal spin_result_latched : unsigned(5 downto 0) := "000000";
+  
+  
+begin
+
+	process(KEY(0))
+		begin
+		if rising_edge(KEY(0)) then
+			spin_result_latched <= spin_result;
+			bet1_value <= SW(8 downto 3);
+		end if;
+ 		
+	end process;
+	
+	spinwheel0: spinwheel port map(CLOCK_27, KEY(2), spin_result );
+	
+	bet1_value <= "110011";
+	bet2_colour <= '1';
+	bet3_dozen <= "01";
+
+	win_detect: win port map(spin_result_latched, bet1_value, bet2_colour, bet3_dozen, bet1_wins, bet2_wins, bet3_wins);
+	LEDG(0) <= bet1_wins;
+	LEDG(1) <= bet2_wins;
+	LEDG(2) <= bet3_wins;
+	
+	
+  --Current Balance Displays
+  disp1: digit7seg port map(unsigned(balance(11 downto 8)), HEX2);
+  disp2: digit7seg port map(unsigned(balance(7 downto 4)), HEX1);
+  disp3: digit7seg port map(unsigned(balance(3 downto 0)), HEX0);
+  
+  --Spin Result Registers
+  disp4: digit7seg port map( "00" & spin_result_latched(5 downto 4), HEX7);
+  disp5: digit7seg port map( spin_result_latched(3 downto 0), HEX6);
+  
+
 END;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
